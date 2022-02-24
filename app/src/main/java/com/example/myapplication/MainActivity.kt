6ae407e9191406e8e77ef31814a5055df7d8491f
Ilya-Yaldinov <90057250+Ilya-Yaldinov@users.noreply.github.com>
2022-02-24 21:49:09 +0500

    2 задание

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,29 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import android.widget.Toast.makeText
+import org.w3c.dom.Text
 import java.io.Console
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val view = findViewById<TextView>(R.id.textView)
-        val student = Student("Илья", "ПрИ-101")
-        view.text=student.toString()
+    }
+
+    fun pressButton(view: View){
+        val edit = findViewById<EditText>(R.id.edit)
+        val view = findViewById<TextView>(R.id.view)
+        if (edit.text.isNotEmpty()){
+            view.text = edit.getText().toString()
+        }else{
+            val button = Toast.makeText(this, "Введите текст", Toast.LENGTH_SHORT)
+            button.show()
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/Student.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication
-
-data class Student(val name: String, val group: String ){
-    public override fun toString(): String {
-        return "Привет, меня зовут $name. Я из $group группы."
-    }
-}
